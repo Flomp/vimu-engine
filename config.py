4cd237fe975fabf9f7fc824ff5a3662fb87e6a14
Christian Beutel <>
2023-02-28 17:23:10 +0100

    makes stripe envs optional

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Settings(BaseSettings):
     pocketbase_url: str
     pocketbase_admin_email: str
     pocketbase_admin_password: str
-    stripe_api_key: str
-    stripe_webhook_secret: str
+    stripe_api_key: str = None
+    stripe_webhook_secret: str = None
     redis_url: str = None
 
     class Config:
