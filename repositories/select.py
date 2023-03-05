72d11ed2bfe0685ac008db6beb26255ae2eccfb7
Christian Beutel <>
2023-03-05 14:53:53 +0100

    make note select work with scores

--- repositories/select.py
+++ repositories/select.py
@@ -14,7 +14,7 @@ class SelectPartsRepository(Repository):
         if in_0 is not None and parts is not None:
             output = stream.Score()
             for i, part in enumerate(in_0.parts):
-                if any([p.lower() == part.partName.lower() or p == str(i) for p in parts]):
+                if any([p.lower() == part.partName.lower() or p == str(i + 1) for p in parts]):
                     output.append(part)
             for key in node.outputs.keys():
                 output_data[key] = output
@@ -35,11 +35,17 @@ class SelectMeasuresRepository(Repository):
 class SelectNotesRepository(Repository):
 
     def process(self, node: EngineNode, input_data: WorkerInputs, output_data: WorkerOutputs):
-        in_0 = input_data.get('in_0')
+        in_0: stream.Stream = input_data.get('in_0')
         notes = node.data.get('data')
 
         if in_0 is not None and notes is not None:
-            output = stream.Stream()
-            output.append(in_0.flat.notes[int(notes[0]):int(notes[1])])
+            output = in_0.cloneEmpty()
+            if isinstance(in_0, stream.Part):
+                output.append(in_0.recurse().notes[int(notes[0]) - 1:int(notes[1])])
+            elif isinstance(in_0, stream.Score):
+                for part in in_0.parts:
+                    output_part = part.cloneEmpty()
+                    output_part.append(part.recurse().notes[int(notes[0]) - 1:int(notes[1])])
+                    output.append(output_part)
             for key in node.outputs.keys():
                 output_data[key] = output
