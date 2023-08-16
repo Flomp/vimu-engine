1aef23b6146d40e31877a5fed76f801f943d84e8
christian.beutel <christian.beutel@moresophy.com>
2023-08-16 11:29:07 +0200

    adds plugin timeout

--- repositories/plugin.py
+++ repositories/plugin.py
@@ -1,3 +1,6 @@
+import asyncio
+import threading
+
 from models.engine import EngineNode, WorkerInputs, WorkerOutputs
 from repositories.repository import Repository
 
@@ -5,7 +8,15 @@ from repositories.repository import Repository
 class PluginRepository(Repository):
     def process(self, node: EngineNode, input_data: WorkerInputs, output_data: WorkerOutputs):
         code = node.data.get('code', '')
-        loc = {}
 
-        exec(code,
-             {'input_data': input_data, 'node': node, 'output_data': output_data}, loc)
+        thread = threading.Thread(target=run_code, args=(code, node, input_data, output_data))
+        thread.start()
+        thread.join(timeout=10)
+
+        if thread.is_alive():
+            raise asyncio.TimeoutError("Execution timed out")
+
+
+def run_code(code, node, input_data, output_data):
+    exec(code,
+         {'input_data': input_data, 'node': node, 'output_data': output_data})
